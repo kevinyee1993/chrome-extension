d3c1196f15969c81bd0b7930c6ac9e1caa139bd5
Kevin Yee <kevinyee1993@gmail.com>
2018-06-12 03:27:29 -0700

    Change pictures on site to puppy

--- content.js
+++ content.js
@@ -1,1 +1,9 @@
 console.log("Everything working!");
+
+let images = document.getElementsByTagName('img');
+
+
+// console.log(images[0].src);
+for(let i = 0; i < images.length; i++) {
+  images[i].src = "https://pbs.twimg.com/profile_images/446566229210181632/2IeTff-V_400x400.jpeg";
+}
